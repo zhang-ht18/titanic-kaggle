4cc0af4ada12eed326524615ac415ac1ffd593c4
zhanghantian <zhanghantian@kuaishou.com>
2021-06-29 10:16:58 +0800

    add test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from scipy.stats import norm
+power_coef = 3
+x = np.arange(0,3,0.01)
+y = (x ** power_coef / 0.8) - (x ** power_coef /1.2)
+rate = norm.cdf(x ** power_coef / 0.8-1) - norm.cdf(x ** power_coef/1.2-1)
+right_side = x ** power_coef / 0.8 - 1
+left_side = x ** power_coef / 1.2 - 1
+
+# plt.plot(x, left_side)
+# plt.plot(x, right_side)
+plt.plot(x, rate)
+plt.show()
